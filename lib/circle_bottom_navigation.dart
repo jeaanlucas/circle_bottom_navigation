c5efaa69bc48098f895efc284efa3c498c6ca4e5
Jean Lucas <jean.camillo@elotech.com.br>
2019-09-17 09:32:51 -0300

    Initial commit

---
+++ lib/circle_bottom_navigation.dart
@@ -0,0 +1,16 @@
+library circle_bottom_navigation;
+
+import 'package:flutter/material.dart';
+
+class CircleBottomNavigation extends StatefulWidget {
+  @override
+  _CircleBottomNavigationState createState() => _CircleBottomNavigationState();
+}
+
+class _CircleBottomNavigationState extends State<CircleBottomNavigation> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
---
+++ test/circle_bottom_navigation_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:circle_bottom_navigation/circle_bottom_navigation.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = CircleBottomNavigation();
+  });
+}
