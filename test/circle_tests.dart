650f979578a5f6111cb6c92faf7e3f1f0d409da1
Jean Lucas <jean.camillo@elotech.com.br>
2019-09-17 11:57:01 -0300

    Implemented test: Circle has correct tabs.

--- test/circle_tests.dart
+++ test/circle_tests.dart
@@ -1,14 +1,15 @@
+import 'package:circle_bottom_navigation/widgets/tab_data.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:circle_bottom_navigation/circle_bottom_navigation.dart';
 
 void main() {
-  Widget makeTestableWidget({
+  Widget makeWidget({
     Widget child,
   }) {
     return MaterialApp(
       theme: ThemeData(
-        primaryColor: Colors.pink,
+        primaryColor: Colors.deepPurple,
         brightness: Brightness.light,
       ),
       home: Scaffold(
@@ -17,4 +18,44 @@ void main() {
       ),
     );
   }
+
+  testWidgets('Circle has correct tabs', (WidgetTester tester) async {
+    CircleBottomNavigation child = CircleBottomNavigation(
+      tabs: [
+        TabData(
+          iconData: Icons.home,
+          title: "Home",
+        ),
+        TabData(
+          iconData: Icons.history,
+          title: "History",
+        ),
+      ],
+      onTabChangedListener: (position) {},
+    );
+
+    await tester.pumpWidget(
+      makeWidget(
+        child: child,
+      ),
+    );
+
+    final homeTabFinder = find.text("Home");
+    expect(homeTabFinder, findsOneWidget);
+
+    final homeIconTabFinder = find.byIcon(Icons.home);
+    expect(homeIconTabFinder, findsNWidgets(2));
+
+    final searchTabFinder = find.text("History");
+    expect(searchTabFinder, findsOneWidget);
+
+    final searchIconTabFinder = find.byIcon(Icons.history);
+    expect(searchIconTabFinder, findsOneWidget);
+
+    final randomTabFinder = find.text("Search");
+    expect(randomTabFinder, findsNothing);
+
+    final randomIconTabFinder = find.byIcon(Icons.search);
+    expect(randomIconTabFinder, findsNothing);
+  });
 }
